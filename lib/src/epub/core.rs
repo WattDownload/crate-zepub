964ba8de14f9a150a06f6da6d6864c710f61a5f8
ZhiFenBL <contact.owner.wattpad@gmail.com>
2026-01-26 07:40:17 +0530

    Update core.rs

--- lib/src/epub/core.rs
+++ lib/src/epub/core.rs
@@ -703,13 +703,13 @@ impl Drop for EpubBook {
 }
 
 impl EpubBook {
-    iepub_derive::option_string_method!(info, creator);
-    iepub_derive::option_string_method!(info, description);
-    iepub_derive::option_string_method!(info, contributor);
-    iepub_derive::option_string_method!(info, date);
-    iepub_derive::option_string_method!(info, format);
-    iepub_derive::option_string_method!(info, publisher);
-    iepub_derive::option_string_method!(info, subject);
+    zepub_derive::option_string_method!(info, creator);
+    zepub_derive::option_string_method!(info, description);
+    zepub_derive::option_string_method!(info, contributor);
+    zepub_derive::option_string_method!(info, date);
+    zepub_derive::option_string_method!(info, format);
+    zepub_derive::option_string_method!(info, publisher);
+    zepub_derive::option_string_method!(info, subject);
     // /
     // / 设置epub最后修改时间
     // /
@@ -720,9 +720,9 @@ impl EpubBook {
     // / epub.set_last_modify("2024-06-28T08:07:07UTC");
     // / ```
     // /
-    iepub_derive::option_string_method!(last_modify);
-    iepub_derive::option_string_method!(generator);
-    iepub_derive::option_string_method!(language);
+    zepub_derive::option_string_method!(last_modify);
+    zepub_derive::option_string_method!(generator);
+    zepub_derive::option_string_method!(language);
 }
 
 // 元数据
@@ -770,7 +770,7 @@ impl EpubBook {
     /// # Examples
     ///
     /// ```
-    /// use iepub::prelude::*;
+    /// use zepub::prelude::*;
     /// let mut epub = EpubBook::default();
     /// epub.add_meta(EpubMetaData::default().with_attr("k", "v").with_text("text"));
     /// ```
